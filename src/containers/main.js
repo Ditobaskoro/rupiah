1bcfd2261ca10f6a1157a09ffe5d6603e3cd3646
Dito Baskoro <dito.anwar@yahoo.com>
2018-09-20 00:59:07 +0800

    init

---
+++ src/containers/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+	constructor(props, context) {
+			super(props, context);
+			this.state = {};
+	}
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from 'containers/main';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Main />, document.getElementById("index"));
